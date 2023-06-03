c25f0abdfda6712e76d3093bb55ec735e13e4ed4
s-roecker <shaun.roecker@gmail.com>
2023-06-03 01:07:01 -0400

    setup user dynamic page

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,7 +1,8 @@
 import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
 import { Suspense } from "react";
-
+import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
 
 
 type Params = {
@@ -10,19 +11,35 @@ type Params = {
     }
 }
 
+
+export async function generateMetadata({ params: { userId } }: Params): Promise<Metadata> {
+    const userData: Promise<User> = getUser(userId)
+    const user: User = await userData;
+
+    return {
+        title: user.name,
+        description: `This is the page of ${user.name}`
+    }
+
+}
+
 const UserPage = async ({ params: { userId }}: Params) => {
     const userData: Promise<User> = getUser(userId);
     const userPostsData: Promise<Post[]> = getUserPosts(userId);
 
-    const [user, userPosts] = await Promise.all([userData, userPostsData])
+    // const [user, userPosts] = await Promise.all([userData, userPostsData])
+    const user = await userData;
+    const userPosts = await userPostsData;
+
+    
 
     return (
         <>
             <h2>{user.name}</h2>
             <br />
             <Suspense fallback={<h2>Loading...</h2>}>
-                <UserPosts posts={userPosts} />
-
+                {/* @ts-expect-error Async Server Component */}
+                <UserPosts promise={userPosts} />
             </Suspense>
         </>
     )
